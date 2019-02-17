63221b76e3226302e232fbada93c5c47ab5a4a49
bwatson <adamwatson08@gmail.com>
2019-02-16 18:39:12 -0600

    believe i have finished the app but cant test yet

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.lab3;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import android.widget.*;
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> secretWords = new ArrayList(Arrays.asList("APPLE", "BANANA", "CHERRY"));
+    String word = "";
+    String shuffledWord = "";
+    Random rand = new Random();
+    int guesses = 0;
+    String strInput;
+    char charInput;
+    String correctGuess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        String randElement = (secretWords.get(rand.nextInt(secretWords.size())));
+        word = randElement;
+
+        ArrayList<String> splitWord = new ArrayList(Arrays.asList(randElement.split("")));
+        Collections.shuffle(splitWord);
+        for (String c : splitWord)
+            shuffledWord += c;
+
+        displayScrambledWord();
+
+
     }
 
     @Override
@@ -49,4 +76,28 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public void displayScrambledWord(){
+        TextView c = (TextView) findViewById(R.id.scrambledWord);
+        c.setText(shuffledWord + " Number of Guesses: " + guesses);
+    }
+
+    public void guess(View v){
+        EditText c = (EditText) findViewById(R.id.guessText);
+        TextView j = (TextView) findViewById(R.id.correctlyGuessed);
+        strInput = c.getText().toString();
+        charInput = strInput.charAt(0);
+        if(charInput == word.charAt(0)){
+            correctGuess = correctGuess + String.valueOf(charInput);
+            j.setText(correctGuess);
+            word.substring(1);
+            c.setText("");
+        }
+        else{
+            guesses ++;
+            c.setText("");
+        }
+
+
+    }
+
 }
